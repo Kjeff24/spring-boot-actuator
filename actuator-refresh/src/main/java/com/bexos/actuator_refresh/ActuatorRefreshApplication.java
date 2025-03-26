6a2e8f15043b46ae56ce6b7fb4b1824a7b0ebdda
Kjeff24 <jeffreyarthur123@gmail.com>
2025-03-26 08:00:00 +0000

    Actuator refresh init

---
+++ actuator-refresh/src/main/java/com/bexos/actuator_refresh/ActuatorRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.bexos.actuator_refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorRefreshApplication.class, args);
+	}
+
+}
---
+++ actuator-refresh/src/test/java/com/bexos/actuator_refresh/ActuatorRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bexos.actuator_refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorRefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
