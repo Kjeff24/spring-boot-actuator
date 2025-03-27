7fb1508a967da79bec309498aecf042da5f47009
Kjeff24 <jeffreyarthur123@gmail.com>
2025-03-27 10:40:00 +0000

    Rename project from actuator_refresh to first_service

--- first_service/src/main/java/com/bexos/actuator_refresh/FirstServiceApplication.java
+++ first_service/src/main/java/com/bexos/actuator_refresh/FirstServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ActuatorRefreshApplication {
+public class FirstServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ActuatorRefreshApplication.class, args);
+		SpringApplication.run(FirstServiceApplication.class, args);
 	}
 
 }
