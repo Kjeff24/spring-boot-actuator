ae18c59c0fd5d7021a0f76bf66dbbf5776144bda
Kjeff24 <jeffreyarthur123@gmail.com>
2025-03-28 12:30:00 +0000

    Remove unrequired methods

--- first_service/src/main/java/com/bexos/actuator_refresh/controller/PropertyController.java
+++ first_service/src/main/java/com/bexos/actuator_refresh/controller/PropertyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/test-a")
+@RequestMapping("/test")
 public class PropertyController {
     private final PropertyService propertyService;
 
